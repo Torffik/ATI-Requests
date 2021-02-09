ef6e12f9663bcdfc3315b3b409a80b69b847e2cd
Malik Pirogov <pirogov.malik@yandex.ru>
2021-02-09 18:44:04 +0500

    Третья задача

--- 1.py
+++ 1.py
@@ -4,22 +4,76 @@ from PIL import Image
 from io import BytesIO
 import os
 
-picture = requests.get(
-    "https://static-maps.yandex.ru/1.x/?ll=55.958727%2C54.735150&spn=1.026457,1.0219&l=map").content
-i = Image.open(BytesIO(picture))
-i.save('загруженное.png')
+
+class Map(pygame.sprite.Sprite):
+    def __init__(self, x=55.958727, y=54.735150):
+        super().__init__(maps)
+        self.x = x
+        self.y = y
+        picture = requests.get(
+            f"https://static-maps.yandex.ru/1.x/?ll={self.x}%2C{self.y}&spn=1.026457,1.0219&l=map").content
+        i = Image.open(BytesIO(picture))
+        i.save('загруженное.png')
+        self.image = pygame.image.load('загруженное.png')
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        if left:
+            karta.x -= 0.1
+        if right:
+            karta.x += 0.1
+        if up:
+            karta.y += 0.1
+        if down:
+            karta.y -= 0.1
+        picture = requests.get(
+            f"https://static-maps.yandex.ru/1.x/?ll={self.x}%2C{self.y}&spn=1.026457,1.0219&l=map").content
+        i = Image.open(BytesIO(picture))
+        i.save('загруженное.png')
+        self.image = pygame.image.load('загруженное.png')
+        self.rect = self.image.get_rect()
+
+
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Покажи мне её!')
+    pygame.display.set_caption('Биг прожект')
     size = width, height = 600, 450
     screen = pygame.display.set_mode(size)
     running = True
-    pic = pygame.image.load('загруженное.png')
-    screen.blit(pic, pic.get_rect())
+    clock = pygame.time.Clock()
+    maps = pygame.sprite.Group()
+    fps = 30
+    karta = Map()
+    left = False
+    right = False
+    down = False
+    up = False
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    up = True
+                elif event.key == pygame.K_DOWN:
+                    down = True
+                elif event.key == pygame.K_LEFT:
+                    left = True
+                elif event.key == pygame.K_RIGHT:
+                    right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    up = False
+                elif event.key == pygame.K_DOWN:
+                    down = False
+                elif event.key == pygame.K_LEFT:
+                    left = False
+                elif event.key == pygame.K_RIGHT:
+                    right = False
+        clock.tick(fps)
+        maps.draw(screen)
+        maps.update()
         pygame.display.flip()
+        screen.fill((0, 0, 0))
     pygame.quit()
 os.remove('загруженное.png')
